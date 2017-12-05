4432cfcd8112b6d819d90fb30737cbdae12cc0ab
matheus-lima92 <lima.eca@gmail.com>
2017-12-05 00:03:28 -0200

    Initial website structure

---
+++ js/dados_iniciais.js
@@ -0,0 +1,204 @@
+var dados_iniciais = [
+	{
+		nome: "Ana Guimarães",
+		prioridade: "p1",
+		apartamento: "162A",
+		vagaSorteada: {
+			id: "--",
+			local: "--"
+		},
+		vaga1: {
+			id: "01",
+			local: "SS1"
+		},
+		vaga2: {
+			id: "02",
+			local: "SS1"
+		},
+		vaga3: {
+			id: "03",
+			local: "SS1"
+		},
+		vaga4: {
+			id: "04",
+			local: "SS1"
+		},
+	},
+	{
+		nome: "Beto de Assis",
+		prioridade: "p1",
+		apartamento: "123B",
+		vagaSorteada: {
+			id: "--",
+			local: "--"
+		},
+		vaga1: {
+			id: "05",
+			local: "SS1"
+		},
+		vaga2: {
+			id: "06",
+			local: "SS1"
+		},
+		vaga3: {
+			id: "07",
+			local: "SS1"
+		},
+		vaga4: {
+			id: "08",
+			local: "SS1"
+		},
+	},
+	{
+		nome: "Carlos Alberto",
+		prioridade: "p1",
+		apartamento: "078D",
+		vagaSorteada: {
+			id: "--",
+			local: "--"
+		},
+		vaga1: {
+			id: "09",
+			local: "SS1"
+		},
+		vaga2: {
+			id: "10",
+			local: "SS1"
+		},
+		vaga3: {
+			id: "11",
+			local: "SS1"
+		},
+		vaga4: {
+			id: "12",
+			local: "SS1"
+		},
+	},
+	{
+		nome: "Daniele de Souza",
+		prioridade: "p1",
+		apartamento: "137C",
+		vagaSorteada: {
+			id: "--",
+			local: "--"
+		},
+		vaga1: {
+			id: "13",
+			local: "SS1"
+		},
+		vaga2: {
+			id: "14",
+			local: "SS1"
+		},
+		vaga3: {
+			id: "15",
+			local: "SS1"
+		},
+		vaga4: {
+			id: "16",
+			local: "SS1"
+		},
+	},
+	{
+		nome: "Elizio Campos",
+		prioridade: "p1",
+		apartamento: "137C",
+		vagaSorteada: {
+			id: "--",
+			local: "--"
+		},
+		vaga1: {
+			id: "17",
+			local: "SS1"
+		},
+		vaga2: {
+			id: "18",
+			local: "SS1"
+		},
+		vaga3: {
+			id: "19",
+			local: "SS1"
+		},
+		vaga4: {
+			id: "20",
+			local: "SS1"
+		},
+	},
+	{
+		nome: "Flávia Ribeiro",
+		prioridade: "p1",
+		apartamento: "117A",
+		vagaSorteada: {
+			id: "--",
+			local: "--"
+		},
+		vaga1: {
+			id: "21",
+			local: "SS1"
+		},
+		vaga2: {
+			id: "22",
+			local: "SS1"
+		},
+		vaga3: {
+			id: "23",
+			local: "SS1"
+		},
+		vaga4: {
+			id: "24",
+			local: "SS1"
+		},
+	},
+	{
+		nome: "Geraldo Gomes",
+		prioridade: "p1",
+		apartamento: "140A",
+		vagaSorteada: {
+			id: "--",
+			local: "--"
+		},
+		vaga1: {
+			id: "25",
+			local: "SS1"
+		},
+		vaga2: {
+			id: "26",
+			local: "SS1"
+		},
+		vaga3: {
+			id: "27",
+			local: "SS1"
+		},
+		vaga4: {
+			id: "28",
+			local: "SS1"
+		},
+	},
+	{
+		nome: "Luciana Albuquerque",
+		prioridade: "p1",
+		apartamento: "123B",
+		vagaSorteada: {
+			id: "--",
+			local: "--"
+		},
+		vaga1: {
+			id: "01",
+			local: "SS1"
+		},
+		vaga2: {
+			id: "02",
+			local: "SS1"
+		},
+		vaga3: {
+			id: "03",
+			local: "SS1"
+		},
+		vaga4: {
+			id: "04",
+			local: "SS1"
+		},
+	},
+
+
+]
---
+++ js/main.js
@@ -0,0 +1,7 @@
+var myApp = angular.module('CSA', []);
+
+myApp.controller('CSAController', ['$scope', '$http', function($scope, $http) {
+
+	console.log("Angular ok!");
+
+}]);
